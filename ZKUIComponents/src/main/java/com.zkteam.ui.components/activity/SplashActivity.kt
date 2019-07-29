34e388d61c4d94b0a5100980e70016457a9cbdd1
WangQing <wangqing11@100tal.com>
2019-07-29 14:58:27 +0800

    1. 启动页面延迟进入的时间，可配置，默认为 3 秒。

--- ZKUIComponents/src/main/java/com.zkteam.ui.components/activity/SplashActivity.kt
+++ ZKUIComponents/src/main/java/com.zkteam.ui.components/activity/SplashActivity.kt
@@ -173,8 +173,10 @@ open class SplashActivity: ZKBaseActivity() {
     }
 
     companion object {
+        //启动界面延迟进入时间
+        const val DELAY_TIME = 3000
+
         private const val FIRST_START = "first_start"
-        private const val DELAY_TIME = 3000
         private const val FLAG_ENTER_MAIN = 0
     }
 }
