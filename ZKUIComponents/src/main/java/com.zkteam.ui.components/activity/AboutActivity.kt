d9baa6a11c9eaad3c4b5d830ddb4017a4ccb493c
WangQing <wangqing11@100tal.com>
2019-07-29 19:39:49 +0800

    1. app 启动默认延迟 0； 2. 去除顶部状态栏的默认色； 3. 添加硬性的 ZKSDK:1.1.3； 4. 关于我们添加 toolbar，并去除全屏样式。

--- ZKUIComponents/src/main/java/com.zkteam.ui.components/activity/AboutActivity.kt
+++ ZKUIComponents/src/main/java/com.zkteam.ui.components/activity/AboutActivity.kt
@@ -3,15 +3,27 @@ package com.zkteam.ui.components.activity
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import com.blankj.utilcode.util.AppUtils
 import com.zkteam.sdk.base.ZKBaseActivity
 import com.zkteam.ui.components.R
 import kotlinx.android.synthetic.main.activity_about.*
-import kotlinx.android.synthetic.main.widget_toolbar.*
 
 class AboutActivity: ZKBaseActivity() {
     override fun getLayoutId(): Int {
         return R.layout.activity_about
+    }
+
+    override fun getToolbar(): Toolbar? {
+        return toolbar_about
+    }
+
+    override fun initToolbar(): Toolbar? {
+        val toolbar = super.initToolbar()
+
+        toolbar!!.title = "关于我们"
+
+        return toolbar
     }
 
     override fun initData(bundle: Bundle?) {
@@ -28,7 +40,6 @@ class AboutActivity: ZKBaseActivity() {
 
     @SuppressLint("SetTextI18n")
     override fun initViews(contentView: View) {
-        tv_title.text = "关于我们 "
         tv_app_name.text = AppUtils.getAppName()
         tv_version.text = "版本号: " + AppUtils.getAppVersionName()
 
--- app/src/main/java/com/zkteam/ui/components/demo/ZKUIApplication.kt
+++ app/src/main/java/com/zkteam/ui/components/demo/ZKUIApplication.kt
@@ -2,11 +2,13 @@ package com.zkteam.ui.components.demo
 
 import com.zkteam.sdk.base.ZKBaseApplication
 import com.zkteam.ui.components.ZKUIManager
+import com.zkteam.ui.components.activity.ZKSplashActivity
 
 class ZKUIApplication : ZKBaseApplication() {
 
     override fun onCreate() {
         super.onCreate()
+        ZKSplashActivity.DELAY_TIME = 0
         val zkUiManager = ZKUIManager.instance
         zkUiManager.setShowGuide(false)
         zkUiManager.setMainActivity(MainActivity::class.java)
